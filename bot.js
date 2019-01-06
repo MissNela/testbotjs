cfcf079550e8e3eec8b5341cab1f03c2e130a86f
MissNela <44345612+MissNela@users.noreply.github.com>
2019-01-06 06:41:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,21 @@
+const Discord = require('discord.js');
+const { Client, Util} = require('discord.js');
+const config = require("./config.json");
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+
+const client = new Client({ disableEveryone: true});
+
+const youtube = new YouTube(config.GOOGLE_API_KEY);
+const PREFIX = config.prefix;
+
+const queue = new Map();
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+
+client.on('ready', () => console.log('I am ready!'));
 
 
 client.on('message', function(message) {
@@ -15,3 +33,5 @@ client.on('message', function(message) {
           message.channel.sendMessage("Invaild command! Try using M]help!")
   }
 });
+
+client.login(process.env.BOT_TOKEN);
