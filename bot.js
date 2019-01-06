ef27d54f43bcca48c4b92627a236ff7adfeb1c2c
MissNela <44345612+MissNela@users.noreply.github.com>
2019-01-06 08:01:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,17 @@ const PREFIX = config.prefix;
 
 var bot = new Discord.Client();
 
+bot.on("ready", function() {
+  console.log("Im here! :/");
+});
+
 bot.on("message", function(message) {
-  console.log(message.content);
-   
+  if (message.author.equals(bot.user)) return;
+  
+  if (message.content == "Hello") {
+    message.channel.sendMessage("Bye");
+  }
+  
 });
 
           
