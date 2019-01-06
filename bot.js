5b72dc048cd3de98adc61f4325b03c373bda2fed
MissNela <44345612+MissNela@users.noreply.github.com>
2019-01-06 07:47:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,38 +1,14 @@
-const Discord = require('discord.js');
-const { Client, Util} = require('discord.js');
-const config = require("./config.json");
-const YouTube = require('simple-youtube-api');
-const ytdl = require('ytdl-core');
+const Discord = require("discord.js");
 
-const client = new Client({ disableEveryone: true});
-
-const youtube = new YouTube(config.GOOGLE_API_KEY);
 const PREFIX = config.prefix;
 
-const queue = new Map();
+var bot = new Discord.Client();
 
-client.on('warn', console.warn);
-
-client.on('error', console.error);
-
-client.on('ready', () => console.log('I am ready!'));
-
-
-client.on('message', function(message) {
-  if (message.author.equals(client.user)) return;
-  
-  if (!message.content.startsWith(PREFIX)) return;
-  
-  var args = message.content.substring(PREFIX.elngth).split(" ");
-  
-  switch (args[0].toLowerCase()) {
-      case "ping":
-          message.channel.sendMessage("Pong! :ping_pong:");
-          break;
-      default:
-          message.channel.sendMessage("Invaild command! Try using M]help!")
-  }
+bot.on("message", function(message) {
+  console.log(message.content);
+   
 });
 
+          
+bot.login(process.env.BOT_TOKEN);
 
-client.login(process.env.BOT_TOKEN);
